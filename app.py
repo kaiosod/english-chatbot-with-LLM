fcf2c3165c41e859fc65844996e31f477186ed32
Kaio Sodré <kaiosodre17@outlook.com>
2024-05-05 22:25:53 -0300

    app.py - update

--- app.py
+++ app.py
@@ -34,9 +34,10 @@ result_ollamaList = subprocess.run("ollama list", shell=True, capture_output=Tru
 if re.search(regex_pattern, result_ollamaList.stdout):
     print("llama 2 is already installed")
 else:
-    print("llama 2 is NOT installed")
+    print("Installing llama 2...")
     subprocess.run("ollama pull llama2", shell=True, capture_output=True, text=True)
 
 
 # Execute main.py
 subprocess.run("python -m chainlit run main.py", shell=True, capture_output=True, text=True)
+print("App Running...")
